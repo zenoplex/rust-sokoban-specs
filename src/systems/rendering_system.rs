bb9f36a65df8f5c55366270b36fa2fc84cbebc51
Gen Ichino <zenoplex@gmail.com>
2021-05-15 20:24:48 +0900

    feat: introduce batch rendering

--- src/systems/rendering_system.rs
+++ src/systems/rendering_system.rs
@@ -1,12 +1,12 @@
-use std::time::Duration;
-
 use crate::{components::*, resources::Gameplay};
 use crate::{constants::*, resources::Time};
 use ggez::{
-    graphics::{self, Color, Image},
+    graphics::{self, spritebatch::SpriteBatch, Color, DrawParam, Image},
     nalgebra, timer, Context,
 };
+use itertools::Itertools;
 use specs::{Join, Read, ReadStorage, System};
+use std::{collections::HashMap, time::Duration};
 
 pub struct RenderingSystem<'a> {
     pub context: &'a mut Context,
@@ -25,17 +25,40 @@ impl<'a> System<'a> for RenderingSystem<'a> {
 
         graphics::clear(self.context, graphics::Color::new(0.95, 0.95, 0.95, 1.0));
 
-        let mut rendering_data: Vec<_> = (&positions, &renderables).join().collect();
-        // sort by z-index
-        rendering_data.sort_by_key(|&k| k.0.z);
+        let rendering_data: Vec<_> = (&positions, &renderables).join().collect();
+        // {z: u8, map: { path: string, draw_params: [] }}
+        let mut rendering_batches: HashMap<u8, HashMap<String, Vec<DrawParam>>> = HashMap::new();
 
         for (position, renderable) in rendering_data.iter() {
             let image = self.get_image(&renderable, time.delta);
             let x = position.x as f32 * TILE_WIDTH;
             let y = position.y as f32 * TILE_WIDTH;
+            let z = position.z;
 
             let draw_params = graphics::DrawParam::new().dest(nalgebra::Point2::new(x, y));
-            graphics::draw(self.context, &image, draw_params).expect("Draw error");
+            rendering_batches
+                .entry(z)
+                .or_default()
+                .entry(image)
+                .or_default()
+                .push(draw_params);
+        }
+
+        for (_z, group) in rendering_batches
+            .iter()
+            .sorted_by(|a, b| Ord::cmp(&a.0, &b.0))
+        {
+            for (image_path, draw_params) in group {
+                let image = Image::new(self.context, image_path).expect("Image error");
+                let mut sprite_batch = SpriteBatch::new(image);
+
+                for draw_param in draw_params.iter() {
+                    sprite_batch.add(*draw_param);
+                }
+
+                graphics::draw(self.context, &sprite_batch, graphics::DrawParam::new())
+                    .expect("Draw error");
+            }
         }
 
         self.draw_text(&gameplay.state.to_string(), 525.0, 80.0);
@@ -66,13 +89,12 @@ impl RenderingSystem<'_> {
         .expect("Draw error");
     }
 
-    pub fn get_image(&mut self, renderable: &Renderable, delta: Duration) -> Image {
+    pub fn get_image(&mut self, renderable: &Renderable, delta: Duration) -> String {
         let path_index = match renderable.kind() {
             RenderableKind::Static => 0,
             RenderableKind::Animated => ((delta.as_millis() % 1000) / 250) as usize,
         };
 
-        let image_path = renderable.path(path_index);
-        Image::new(self.context, image_path).expect("Image Error")
+        renderable.path(path_index)
     }
 }
